2f3fd872fd3e384583075dbf0d4702bbc197d452
YourMomLoves MyKeyboard <ymlmkb@gmail.com>
2021-09-18 16:10:22 -0500

    ymlmkb
    first stab at keeb.io BDN9.

---
+++ keyboards/keebio/bdn9/keymaps/ymlmkb/keymap.c
@@ -0,0 +1,97 @@
+#include QMK_KEYBOARD_H
+#include "ymlmkb.h"
+
+#define _BASE 0
+#define _YRGB 1
+#define _YRST 2
+
+enum encoder_names {
+  _LEFT,
+  _RIGHT,
+  _MIDDLE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT(
+    MO(_YRGB),    MO(_YRGB), MO(_YRGB),
+    S(C(KC_TAB)), C(KC_TAB), KC_PGUP,
+    S(A(KC_TAB)), A(KC_TAB), KC_PGDN
+  ),
+
+  [_YRGB] = LAYOUT(
+    KC_TRNS,   KC_TRNS, KC_TRNS,
+    MO(_YRST), RGB_TOG, RGB_MOD,
+    XXXXXXX,   RGB_SPD, RGB_SPI
+  ),
+
+  [_YRST] = LAYOUT(
+    KC_TRNS,   KC_TRNS, KC_TRNS,
+    KC_TRNS,   XXXXXXX, XXXXXXX,
+    RESET,     XXXXXXX, XXXXXXX
+  ),
+};
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+
+  if (IS_LAYER_ON(_BASE)) {
+    if (index == _LEFT) {
+      if (clockwise) {
+        tap_code(KC_MS_WH_UP);
+      } else {
+        tap_code(KC_MS_WH_DOWN);
+      }
+    }
+    else if (index == _MIDDLE) {
+      if (clockwise) {
+        tap_code16(S(KC_SCLN));
+        tap_code(KC_N);
+        tap_code(KC_ENT);
+      } else {
+        tap_code16(S(KC_SCLN));
+        tap_code16(S(KC_N));
+        tap_code(KC_ENT);
+      }
+    }
+    else if (index == _RIGHT) {
+      if (clockwise) {
+        tap_code16(C(KC_A));
+        tap_code((KC_N));
+      } else {
+        tap_code16(C(KC_A));
+        tap_code((KC_P));
+      }
+    }
+  }
+
+  if (IS_LAYER_ON(_YRGB)) {
+    if (index == _LEFT) {
+      if (clockwise) {
+        rgb_matrix_increase_val();
+      } else {
+        rgb_matrix_decrease_val();
+      }
+    }
+    else if (index == _MIDDLE) {
+      if (clockwise) {
+        rgb_matrix_increase_hue();
+      } else {
+        rgb_matrix_decrease_hue();
+      }
+    }
+    else if (index == _RIGHT) {
+      if (clockwise) {
+        rgb_matrix_increase_sat();
+      } else {
+        rgb_matrix_decrease_sat();
+      }
+    }
+  }
+
+}
+
+// KC_MPLY
+// KC_MFFD
+// KC_MRWD
+// KC__MUTE
+// KC__VOLUP
+// KC__VOLDOWN
